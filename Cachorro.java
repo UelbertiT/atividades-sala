6e0872e31945edab6908bde9ae57cf43d4f6f14e
UelbertiT <127126072+UelbertiT@users.noreply.github.com>
2023-03-08 20:28:49 -0300

    Ativade-poo

---
+++ Cachorro.java
@@ -0,0 +1,15 @@
+package com.mycompany.exercicio1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cachorro {
+
+    public String nome;
+    public String raca;
+    public String cor;
+    public int idade;
+    public int peso;
+
+}
---
+++ testeCachorro.java
@@ -0,0 +1,53 @@
+package com.mycompany.exercicio1;
+
+/**
+ *
+ * @author Uelberti
+ */
+public class testeCachorro {
+
+    public static void main(String[] args) {
+
+        Cachorro cachorro1 = new Cachorro();
+        
+        cachorro1.nome = "Pluto";
+        cachorro1.idade = 10;
+        cachorro1.cor = "Amarelo";
+        cachorro1.peso = 20;
+        cachorro1.raca = "pastor alemão";
+        
+        Cachorro cachorro2 = new Cachorro();
+        
+        cachorro2.nome = "Rex";
+        cachorro2.idade = 12;
+        cachorro2.cor = "preto";
+        cachorro2.peso = 24;
+        cachorro2.raca = "Buldogue";
+        
+        Cachorro cachorro3 = new Cachorro();
+        
+        cachorro3.nome = "bob";
+        cachorro3.idade = 4;
+        cachorro3.cor = "Amarelo";
+        cachorro3.peso = 12;
+        cachorro3.raca = "Labrador";
+        
+        Cachorro cachorro4 = new Cachorro();
+        
+        cachorro4.nome = "task";
+        cachorro4.idade = 2;
+        cachorro4.cor = "marrom";
+        cachorro4.peso = 18;
+        cachorro4.raca = "Goldem";
+        
+        Cachorro cachorro5 = new Cachorro();
+        
+        cachorro5.nome = "belinha";
+        cachorro5.idade = 7;
+        cachorro5.cor = "preto";
+        cachorro5.peso = 14;
+        cachorro5.raca = "Husky Siberiano";
+
+    }
+
+}
